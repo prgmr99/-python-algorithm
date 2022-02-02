862c3a232685eeae1b307908170b1f429d361f92
prgmr99 <johnyeom24@naver.com>
2022-02-02 22:00:30 +0900

    implementation 3

--- implementation.py
+++ implementation.py
@@ -36,7 +36,7 @@
 #         if plan==move_types[i]:
 #             nx=x+dx[i]
 #             ny=y+dy[i]
-#     if nx<1 or ny<1 or nx>x or ny>n:
+#     if nx<1 or ny<1 or nx>n or ny>n:
 #         continue
 #     x,y=nx,ny
 # print(x,y)
@@ -48,7 +48,49 @@
 # for i in range(n+1):
 #     for j in range(60):
 #         for k in range(60):
-#             if k%10==3 or k//10==3 or j%10==3 or j//10==3 or i%10==3 or i//10==3:
+#             if k%10==3 or k//10==3 or j%10==3 or j//10==3 or i%10==3 or i//10==3:  # c++에서는 이렇게 사용 -> 문자열로 변경이 어렵기 때문
 #                 cnt+=1
 # print(cnt)
 
+# 해설
+# h=int(input())
+# count=0
+# for i in range(h+1):
+#     for j in range(60):
+#         for k in range(60):
+#             if '3' in str(i)+str(j)+str(k): # 내가 두 개의 조건으로 사용한 것을 문자열로 바꿔 하나의 조건으로 만듬
+#                 count+=1
+# print(count)
+
+# <문제 3>
+# 내 풀이
+# coord=input()
+# dx=[2,2,1,1,-1,-1,-2,-2]
+# dy=[-1,1,-2,2,-2,2,-1,1]
+# temp_y=ord(coord[0])-96
+# cnt=0
+# for i in range(8):
+#     ny=temp_y+dy[i]
+#     nx=int(coord[1])+dx[i]
+#     if ny<1 or nx<1 or nx>8 or ny>8:
+#         continue
+#     cnt+=1
+# print(cnt)
+
+# 해설
+# input_data=input()
+# row=int(input_data[1])
+# column=int(ord(input_data[0]))-int(ord('a'))+1
+
+# steps=[(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+
+# result=0
+# for step in steps:
+#     next_row=row+step[0]
+#     next_column=column+step[1]
+#     if next_row>=1 and next_row<=8 and next_column>=1 and next_column<=8:
+#         result+=1
+# print(result)
+
+# <문제 3>
+# 내 풀이
