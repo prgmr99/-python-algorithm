3a1e751ef7da7b688097afef3093ee04999c78f0
prgmr99 <johnyeom24@naver.com>
2022-02-03 14:31:52 +0900

    dfs,bfs question 1

--- DFS&BFS.py
+++ DFS&BFS.py
@@ -68,3 +68,72 @@
 # print(gcd(192,162))
 
 #---------------------------------------------------------------------
+# <DFS> Depth_First Search
+# 깊이 우선 탐색: 깊은 부분을 우선적으로 탐색하는 알고리즘
+# 스택 자료구조(혹은 재귀함수)를 이용한다.
+# 1. 탐색 시작 노드를 스택에 삽입하고 방문 처리를 한다.
+# 2. 스택의 최상단 노드에 방문하지 않은 인접한 노드가 하나라도 있으면
+# 그 노드를 스택에 넣고 방문 처리한다. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼낸다.
+# 3. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복한다.
+
+# 예제 1
+# def dfs(graph,v,visited):
+#     visited[v]=True
+#     print(v,end=' ')
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph,i,visited)
+
+# graph=[
+#     [],
+#     [2,3,8], 1(인덱스도 1로 맞춤)번 노드가 연결된 노드
+#     [1,7],   2번 노드가 연결된 노드
+#     [1,4,5],
+#     [3,5],
+#     [3,4],
+#     [7],
+#     [2,6,8],
+#     [1,7]
+# ]
+# visited=[False]*9
+# dfs(graph,1,visited)
+
+# <BFS> Breadth_First Search
+# 너비 우선 탐색: 가까운 노드부터 우선적으로 탐색하는 알고리즘
+# 큐 자료구조 이용
+# 1. 탐색 시작 노드를 큐에 삽입하고 방문 처리를 한다.
+# 2. 큐에서 노드를 꺼낸 뒤에 해당 노드의 인접 노드 중에서 방문하지 않은 노드를 
+# 모두 큐에 삽입하고 방문 처리한다.
+# 3. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복한다.
+
+# 예제 1
+# from collections import deque
+# def bfs(graph,start,visited):
+#     queue=deque([start])
+#     visited[start]=True
+#     while queue:
+#         v=queue.popleft()
+#         print(v,end=' ')
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i]=True
+                
+# graph=[
+#     [],
+#     [2,3,8], 
+#     [1,7],  
+#     [1,4,5],
+#     [3,5],
+#     [3,4],
+#     [7],
+#     [2,6,8],
+#     [1,7]
+# ]
+
+# visited=[False]*9
+# bfs(graph,1,visited)
+
+# <문제 1>
+# 내 풀이
+n,m=int(input().split())
